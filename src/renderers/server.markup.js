a72a9ec18b427d4704980ac7d49f2633d22f4f9c
Vlad Nicula <vladnicula.vn@gmail.com>
2015-10-24 13:28:29 +0100

    Added basic markup for server page

---
+++ src/renderers/server.markup.js
@@ -0,0 +1,15 @@
+'use strict'
+
+export default function ( htmlMarkup ) {
+	return `<!doctype>
+<html>
+<head>
+	<title>React Real Estate Investment</title>
+</head>
+<body>
+	<div id='root'>${htmlMarkup}</div>
+	<script type='text/javascript' src='/dist/library.bundle.js'></script>
+	<script type='text/javascript' src='/dist/app.bundle.js'></script>
+</body>
+</html>`
+}
--- src/renderers/server.renderer.js
+++ src/renderers/server.renderer.js
@@ -3,11 +3,11 @@ import { renderToString } from "react-dom/server"
 
 import Dummy from "../containers/dummy"
 
+import markupFn from "./server.markup"
+
 export default function universalServerRenderer ( req, res, next ) {
 
 	var reactHTMLString = renderToString(<Dummy/>)
 
-	console.log("htmlString\n",reactHTMLString)
-
-	res.status(200).send(reactHTMLString)
+	res.status(200).send(markupFn(reactHTMLString))
 }
