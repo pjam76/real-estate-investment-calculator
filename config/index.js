83b8f4586d18fb98b07bb44b9be5b03a27e6f743
Vlad Nicula <vladnicula.vn@gmail.com>
2015-10-24 12:47:39 +0100

    Main entry point of server ready

---
+++ config/index.js
@@ -0,0 +1,2 @@
+export const APPLICATION_PORT = 3333
+export const API_PORT = 9999
--- server/index.js
+++ server/index.js
@@ -1,3 +1,19 @@
-import express from "express"
+'use strict'
 
-console.log("express and babel running")
+import express from "express"
+import { Server } from "http"
+
+import { APPLICATION_PORT } from "../config"
+
+
+var app = express()
+var appserver = Server(app)
+
+app.get("/*", ( req, res, next ) => {
+	console.log("main universal server routing point")
+	res.sendStatus(200)
+})
+
+appserver.listen(APPLICATION_PORT, () => {
+	console.log(`app listening to on 0.0.0.0:${APPLICATION_PORT}`)
+})
